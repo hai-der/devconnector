0c5b3f211e5c79f1ebb2952bbcf72c5604ad949a
Haider <hai.tiwana@gmail.com>
2020-02-06 18:07:44 -0800

    configure register route with validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route       GET api/users
-// @desc        test route
+// @route       POST api/users
+// @desc        Register user
 // @access      public
-router.get('/', (req, res) => {
-  res.send('users endpoint');
-});
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmail(),
+    check('username', 'Username must be a valid email').isEmail(),
+    check('password', 'Password must be 6 or more characters').isLength({
+      min: 6
+    })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    console.log(req.body);
+    res.send('users endpoint');
+  }
+);
 
 module.exports = router;
