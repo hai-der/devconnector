e48131d6014cbca668da6e31c8dfbb8395dcd206
Haider <hai.tiwana@gmail.com>
2020-02-27 12:07:45 -0800

    add Post API routes

---
+++ models/Post.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const PostSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: String,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: String,
+    default: Date.now
+  }
+});
+
+module.exports = Post = mongoose.model('Post', PostSchema);
