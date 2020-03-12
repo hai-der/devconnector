33a1a78682b0a5fef7c571b1ddcd4f940698f1c7
Haider <hai.tiwana@gmail.com>
2020-03-12 00:20:04 -0700

    create protected route for dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { loadUser } from './redux/actions/auth.actions';
-import setAuthToken from './utils/setAuthToken';
+import PrivateRoute from './components/routing/PrivateRoute';
 import NavBar from './components/layout/NavBar';
 import Landing from './components/layout/Landing';
+import Dashboard from './components/dashboard/Dashboard';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
-import store from './store';
 import './App.css';
+
+// Redux
+import { Provider } from 'react-redux';
+import { loadUser } from './redux/actions/auth.actions';
+import setAuthToken from './utils/setAuthToken';
+import store from './store';
 
 if (localStorage.token) setAuthToken(localStorage.token);
 
@@ -29,6 +33,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
